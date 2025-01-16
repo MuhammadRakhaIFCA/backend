4e84082df097183389f9dba5d45bd34ddcba27cc
muhammad athaillah <muhammad.athaillah@ifca.co.id>
2025-01-16 12:32:59 +0700

    change port to 5000

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { UnauthorizedExceptionFilter } from './customFilters/unauthorized-exception.filter';
 
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT ?? 5000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
